82d43f895da4bb28e9654897915c3c06a731e827
Gibson Kemboi <gibsonsgibson88@gmail.com>
2023-09-27 13:52:55 +0000

    Sep 27, 2023, 4:52 PM

--- callbackUrl/index.ts
+++ callbackUrl/index.ts
@@ -5,3 +5,17 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 admin.initializeApp(functions.config().firebase)
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+app.use(bodyParser.json());
+app.disable('x-powered-by');
+
+
+app.post('/callback', async (req: express.Request, res: express.Response) => {
+    const response = { 'ResultCode': 0, 'ResultDesc': 'Success' };
+
+   re 
+});

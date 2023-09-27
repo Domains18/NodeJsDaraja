2a32e5c5569d008dd8cd62e69ee462a27be98e1a
Gibson Kemboi <gibsonsgibson88@gmail.com>
2023-09-27 14:18:30 +0000

    Sep 27, 2023, 5:18 PM

--- callbackUrl/index.ts
+++ callbackUrl/index.ts
@@ -1,49 +1,51 @@
 import functions from 'firebase-functions';
-import admin from 'firebase-admin'
+import admin from 'firebase-admin';
 
+admin.initializeApp(functions.config().firebase);
 
-
-admin.initializeApp(functions.config().firebase)
 import express from 'express';
 import bodyParser from 'body-parser';
 
-
 const app = express();
+
 app.use(bodyParser.json());
 app.disable('x-powered-by');
 
+app.post('/callback', async (req, res) => {
+  const response = {
+    ResultCode: 0,
+    ResultDesc: 'Success',
+  };
 
-app.post('/callback', async (req: express.Request, res: express.Response) => {
-    const response = { 'ResultCode': 0, 'ResultDesc': 'Success' };
+  res.status(200).json(response);
 
-    res.status(200).json(response);
+  const requestBody = req.body;
+  const myPayload = JSON.stringify(requestBody);
 
-    const requestBody = req.body;
-    const myPayload = JSON.stringify(requestBody);
+  console.log(myPayload);
 
-    console.log(myPayload);
+  const topicId = requestBody.Body.stkCallback.CheckoutRequestID;
 
-    let topicId = requestBody.Body.stkCallback.CheckoutRequestID; '
+  const sentPayload = {
+    data: {
+      myPayload,
+    },
+    Body: {
+      stkCallback: {
+        MerchantRequestID: requestBody.Body.stkCallback.MerchantRequestID,
+        CheckoutRequestID: requestBody.Body.stkCallback.CheckoutRequestID,
+        ResultCode: requestBody.Body.stkCallback.ResultCode,
+        ResultDesc: requestBody.Body.stkCallback.ResultDesc,
+      },
+    },
+    topic: topicId,
+  };
 
-    const sentPayload = {
-        data: {
-            myPayload
-        },
-        "Body": {
-            "stkCallback": {
-                "MerchantRequestID": requestBody.Body.stkCallback.MerchantRequestID,
-                "CheckoutRequestID": requestBody.Body.stkCallback.CheckoutRequestID,
-                "ResultCode": requestBody.Body.stkCallback.ResultCode,
-                "ResultDesc": requestBody.Body.stkCallback.ResultDesc
-            }
-        },
-        "topic": topicId
-
-    };
-    return admin.messaging().send(sentPayload)
-        .catch(error => {
-            console.error(error)
-        })
+  try {
+    await admin.messaging().send(sentPayload);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 exports.api = functions.https.onRequest(app);

3f41edb948f28e04892b8b1d68a24280c1e49431
Gibson Kemboi <gibsonsgibson88@gmail.com>
2023-09-27 14:31:18 +0000

    Sep 27, 2023, 5:31 PM

--- callbackUrl/index.ts
+++ callbackUrl/index.ts
@@ -1,40 +1,19 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
 import express from 'express';
-import * as bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
-admin.initializeApp();
+
 
 const app = express();
 app.use(bodyParser.json());
-app.disable('x-powered-by');
+const port = 3000;
 
-app.post('/callback', async (req: express.Request, res: express.Response) => {
-  try {
-    const requestBody = req.body;
-    const topicId = requestBody.Body.stkCallback.CheckoutRequestID;
 
-    // Send a success response to the M-Pesa API
-    const response = { ResultCode: 0, ResultDesc: 'Success' };
-    res.status(200).json(response);
 
-    // Send the payload to Firebase Cloud Messaging (FCM)
-    const sentPayload = {
-      data: {
-        myPayload: JSON.stringify(requestBody),
-      },
-      notification: {
-        title: 'M-Pesa Callback',
-        body: 'Payment received',
-      },
-      topic: topicId,
-    };
-
-    await admin.messaging().send(sentPayload);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-export const api = functions.https.onRequest(app);
+app.post('/mpesa/callback', (req, res) => {
+    console.log(req.body);
+    res.send('Hello World!');
+});

f5cad8c758c2100019c5ffe4082eaba715bd787b
Gibson Kemboi <gibsonsgibson88@gmail.com>
2023-10-02 10:52:57 +0300

    complete

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,7 @@
 const expressAsyncHandler = require('express-async-handler');
 const moment = require('moment');
+const axios = require('axios');
+const createToken =require('../middleware/accessMiddleware').createToken;
 
 
 
@@ -12,4 +14,36 @@ const makeStkPush = expressAsyncHandler(async (req, res) => {
         return;
     }
     const shortCode = "174379";
+    const passkey = createToken(alphauser, alphapass);
+    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+
+    const date = moment().format("YYYYMMDDHHmmss");
+    const timestamp = new Buffer.from(date).toString("base64");
+
+    const password = new Buffer.from(shortCode + passkey + timestamp).toString("base64");
+    const data = {
+        BusinessShortCode: shortCode,
+        Password: password,
+        Timestamp: timestamp,
+        TransactionType: "CustomerPayBillOnline",
+        Amount: amount,
+        PartyA: phoneNumber,
+        PartyB: shortCode,
+        PhoneNumber: phoneNumber,
+        CallBackURL: "https://sandbox.safaricom.co.ke/mpesa/", //replace with your own
+        AccountReference: "Test",
+        TransactionDesc: "Test"
+    };
+    await axios.post(url, data, {
+        headers: {
+            Authorization: "Bearer " + token
+        }
+    }).then((data) => {
+        res.json(data.data);
+    }).catch((err) => {
+        res.status(500).json({ message: err.message });
+    });
 });
+
+
+exports.makeStkPush = makeStkPush;
--- index.js
+++ index.js
@@ -8,15 +8,46 @@ const fs = require('fs');
 const dotenv = require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
-
+const { errorHandler } = require('./middleware/errorHandler');
 const port = process.env.PORT || 5000;
 const hostname = 'localhost';
 
+app.use(errorHandler);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.get('/', (req, res) => {
+    if (req.accepts('html')) {
+        res.sendFile(__dirname + '/public/index.html');
+        return;
+    } else if (req.accepts('json')) {
+        res.json({ message: "Welcome to the MPESA API" });
+        return;
+    } else {
+        res.send('Welcome to the MPESA API');
+    }
+}
+);
+//static files to use css
+app.use(express.static('public'));
+
+
+const authentication = require('./routes/authentication');
+app.use('/api/v1', authentication);
+
+app.get('/callback', (req, res) => {
+    const { Body, To, From } = req.query;
+    const message = { Body, To, From };
+    fs.writeFile('message.json', JSON.stringify(message), (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Message saved');
+    });
+    res.send('Message saved');
+});
 
 // Start the server
 server.listen(port, hostname, () => {

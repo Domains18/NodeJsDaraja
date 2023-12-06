ccb62dd0261e0efa8db139e5db27b95fa1a8d4df
Domains18 <gibsonsgibson88@gmail.com>
2023-12-06 11:54:50 +0000

    Dec 6, 2023, 2:54 PM

--- controllers/validators.js
+++ controllers/validators.js
@@ -2,13 +2,14 @@ const { fetchTransactionByMerchantRequestID, Transaction } = require("../mongoos
 
 const validateTransaction = async (req, res, next) => {
     const { payload } = req.body;
-    console.log(payload.MerchantRequestID);
+    const MerchantRequestID = payload.MerchantRequestID;
     try {
         const transaction = await Transaction.findOne({ MerchantRequestID: MerchantRequestID });
         if (transaction == null) {
             console.log("Cannot find transaction");
             return res.status(404).json({ message: 'Cannot find transaction' });
         }
+        console.log("transaction", transaction);
         res.status(200).json({ transaction });
     } catch (error) {
 

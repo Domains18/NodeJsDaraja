86863aed37d05c0a2faac1697cc393e929e2b39d
Domains18 <gibsonsgibson88@gmail.com>
2024-10-24 00:25:43 +0300

    Remove unused AppController, AppService, and related tests; update main.ts for global validation and CORS settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+
+
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+    app.enableCors();
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
+
 bootstrap();

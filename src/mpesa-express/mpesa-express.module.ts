a94ddd17704707a12e3efca8b28e3bb127ccd546
Domains18 <gibsonsgibson88@gmail.com>
2024-10-26 21:19:46 +0300

    Enhance MpesaExpress functionality with Prisma integration, add transaction status management, and refactor callback handling

--- src/mpesa-express/mpesa-express.module.ts
+++ src/mpesa-express/mpesa-express.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MpesaExpressService } from './mpesa-express.service';
 import { MpesaExpressController } from './mpesa-express.controller';
 import { AuthService } from 'src/services/auth.service';
+import { PrismaService } from 'src/services/prisma.service';
 
 @Module({
     imports: [],
     controllers: [MpesaExpressController],
-    providers: [MpesaExpressService, AuthService],
+    providers: [MpesaExpressService, AuthService, PrismaService],
 })
 export class MpesaExpressModule {}
